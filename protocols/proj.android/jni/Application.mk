e9104b0b391ee443b0f1e5145ce7c94af95a28b1
Naoyuki Totani <n_totani@septeni-original.co.jp>
2014-05-30 22:01:32 +0900

    build settings for android

--- protocols/proj.android/jni/Application.mk
+++ protocols/proj.android/jni/Application.mk
@@ -4,5 +4,5 @@ APP_STL := gnustl_static
 APP_CPPFLAGS += -frtti
 APP_MODULES := PluginProtocolStatic
 APP_ABI :=armeabi armeabi-v7a
-#APP_ABI :=x86
+APP_ABI :=x86
 #APP_ABI :=mips mips-r2 mips-r2-sf armeabi
