cabbb717ec71f884ff09ec0af6ddc7093f864718
Naoyuki Totani <n_totani@septeni-original.co.jp>
2014-06-04 19:19:36 +0900

    ignore unuse plugin

--- tools/config.sh
+++ tools/config.sh
@@ -1,5 +1,5 @@
 #define plugins array 
-export ALL_PLUGINS=("flurry" "umeng" "admob" "twitter" "facebook" "googleplay")
+export ALL_PLUGINS=("flurry" "admob" "twitter" "facebook" "googleplay")
 
 # define the plugin root directory & publish target directory
 export TARGET_DIR_NAME="publish"
