98cd0baadabfb7de28fa7171b2089719002c4e48
Naoyuki Totani <n_totani@septeni-original.co.jp>
2014-07-01 22:37:41 +0900

    realtime connect

--- plugins/googleplay/proj.android/src/org/cocos2dx/plugin/UserGoogleplay.java
+++ plugins/googleplay/proj.android/src/org/cocos2dx/plugin/UserGoogleplay.java
@@ -24,25 +24,41 @@ THE SOFTWARE.
 package org.cocos2dx.plugin;
 
 import java.util.Hashtable;
+import java.util.List;
+import java.util.ArrayList;
 
+import android.os.Bundle;
 import android.app.Activity;
 import android.content.Context;
 import android.content.Intent;
 import android.util.Log;
 import android.preference.PreferenceManager.OnActivityResultListener;
+import android.view.WindowManager;
 
+import com.google.android.gms.games.Games;
+import com.google.android.gms.games.GamesStatusCodes;
+import com.google.android.gms.games.GamesActivityResultCodes;
+import com.google.android.gms.games.multiplayer.Multiplayer;
+import com.google.android.gms.games.multiplayer.realtime.Room;
+import com.google.android.gms.games.multiplayer.realtime.RoomConfig;
+import com.google.android.gms.games.multiplayer.realtime.RoomUpdateListener;
+import com.google.android.gms.games.multiplayer.realtime.RealTimeMessage;
+import com.google.android.gms.games.multiplayer.realtime.RealTimeMessageReceivedListener;
+import com.google.android.gms.games.multiplayer.realtime.RoomStatusUpdateListener;
 import com.google.example.games.basegameutils.GameHelper;
 
 import org.cocos2dx.lib.Cocos2dxHelper;
 
-public class UserGoogleplay implements InterfaceUser, GameHelper.GameHelperListener, OnActivityResultListener, Cocos2dxHelper.OnActivityStartStopListener, RoomUpdateListener {
+public class UserGoogleplay implements InterfaceUser, GameHelper.GameHelperListener, OnActivityResultListener, Cocos2dxHelper.OnActivityStartStopListener, RoomUpdateListener, RealTimeMessageReceivedListener, RoomStatusUpdateListener {
 
+    private static final int RC_SELECT_PLAYERS = 10000;
     private static final int RC_WAITING_ROOM = 10002;
     private static final String LOG_TAG = "UserGoogleplay";
     private static Activity mContext = null;
     private static UserGoogleplay mGoogleplay = null;
     private static boolean bDebug = false;
     private GameHelper mGameHelper;
+    private String mRoomId;
 
     protected static void LogE(String msg, Exception e) {
         Log.e(LOG_TAG, msg, e);
@@ -145,64 +161,76 @@ public class UserGoogleplay implements InterfaceUser, GameHelper.GameHelperListe
         mGameHelper.onStop();
     }
 
-    public void createQuickStartRoom() {
-        // auto-match criteria to invite one random automatch opponent.  
-        // You can also specify more opponents (up to 3). 
-        Bundle am = RoomConfig.createAutoMatchCriteria(1, 1, 0);
-
-        // build the room config:
-        RoomConfig.Builder roomConfigBuilder = makeBasicRoomConfigBuilder();
-        roomConfigBuilder.setAutoMatchCriteria(am);
-        RoomConfig roomConfig = roomConfigBuilder.build();
-
-        // create room:
-        Games.RealTimeMultiplayer.create(getApiClient(), roomConfig);
-
-        // prevent screen from sleeping during handshake
-        getWindow().addFlags(WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON);
-    }
-
-    @Override
-    public void onRoomCreated(int statusCode, Room room) {
-        if (statusCode != GamesStatusCodes.STATUS_OK) {
-            // let screen go to sleep
-            getWindow().clearFlags(WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON);
-
-            // show error message, return to main screen.
-        }
-        // get waiting room intent
-        Intent i = Games.RealTimeMultiplayer.getWaitingRoomIntent(getApiClient(), room, Integer.MAX_VALUE);
-        startActivityForResult(i, RC_WAITING_ROOM);
-    }
-
     @Override
     public void onJoinedRoom(int statusCode, Room room) {
+        LogD("onJoinedRoom");
         if (statusCode != GamesStatusCodes.STATUS_OK) {
             // let screen go to sleep
-            getWindow().clearFlags(WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON);
+            //mContext.getWindow().clearFlags(WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON);
 
             // show error message, return to main screen.
+        } else {
+            mRoomId = room.getRoomId();
+            // get waiting room intent
+            Intent i = Games.RealTimeMultiplayer.getWaitingRoomIntent(mGameHelper.getApiClient(), room, Integer.MAX_VALUE);
+            mContext.startActivityForResult(i, RC_WAITING_ROOM);
         }
-        // get waiting room intent
-        Intent i = Games.RealTimeMultiplayer.getWaitingRoomIntent(getApiClient(), room, Integer.MAX_VALUE);
-        startActivityForResult(i, RC_WAITING_ROOM);
+    }
+
+    @Override
+    public void onLeftRoom(int statusCode, String roomId) {
+        LogD("onLeftRoom");
     }
 
     @Override
     public void onRoomConnected(int statusCode, Room room) {
+        LogD("onRoomConnected");
         if (statusCode != GamesStatusCodes.STATUS_OK) {
             // let screen go to sleep
-            getWindow().clearFlags(WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON);
+            //mContext.getWindow().clearFlags(WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON);
 
             // show error message, return to main screen.
         }
     }
 
     @Override
-    public boolean onActivityResult(int requestCode, int resultCode, Intent data) {
+    public void onRoomCreated(int statusCode, Room room) {
+        LogD("onRoomCreated");
+        if (statusCode != GamesStatusCodes.STATUS_OK) {
+            // let screen go to sleep
+            //mContext.getWindow().clearFlags(WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON);
+
+            // show error message, return to main screen.
+        } else {
+            mRoomId = room.getRoomId();
+            // get waiting room intent
+            Intent i = Games.RealTimeMultiplayer.getWaitingRoomIntent(mGameHelper.getApiClient(), room, Integer.MAX_VALUE);
+            mContext.startActivityForResult(i, RC_WAITING_ROOM);
+        }
+    }
+
+    @Override public void onConnectedToRoom(Room room) { LogD("onConnectedToRoom"); }
+    @Override public void onDisconnectedFromRoom(Room room) { LogD("onDisconnectedFromRoom"); }
+    @Override public void onP2PConnected(String participantId) { LogD("onP2PConnected"); }
+    @Override public void onP2PDisconnected(String participantId) { LogD("onP2PDisconnected"); }
+    @Override public void onPeerDeclined(Room room, List<String> participantIds) { LogD("onPeerDeclined"); }
+    @Override public void onPeerInvitedToRoom(Room room, List<String> participantIds) { LogD("onPeerInvitedToRoom"); }
+    @Override public void onPeerJoined(Room room, List<String> participantIds) { LogD("onPeerJoined"); }
+    @Override public void onPeerLeft(Room room, List<String> participantIds) {
+        LogD("onPeerLeft"); 
+        UserWrapper.onActionResult(mGoogleplay, UserWrapper.ACTION_RET_LOGOUT_SUCCEED, "onLeft");
+    }
+    @Override public void onPeersConnected(Room room, List<String> participantIds) { LogD("onPeersConnected"); }
+    @Override public void onPeersDisconnected(Room room, List<String> participantIds) { LogD("onPeersDisconnected"); }
+    @Override public void onRoomAutoMatching(Room room) { LogD("onRoomAutoMatching"); }
+    @Override public void onRoomConnecting(Room room) { LogD("onRoomConnecting"); }
+    
+    @Override
+    public boolean onActivityResult(int request, int response, Intent data) {
+        mGameHelper.onActivityResult(request, response, data);
         if (request == RC_WAITING_ROOM) {
+            LogD("onActivityResult");
             if (response == Activity.RESULT_OK) {
-                // (start game)
                 UserWrapper.onActionResult(mGoogleplay, UserWrapper.ACTION_RET_LOGOUT_SUCCEED, "onMatch");
             } else if (response == Activity.RESULT_CANCELED) {
                 // Waiting room was dismissed with the back button. The meaning of this
@@ -211,16 +239,125 @@ public class UserGoogleplay implements InterfaceUser, GameHelper.GameHelperListe
                 // continue to connect in the background.
 
                 // in this example, we take the simple approach and just leave the room:
-                Games.RealTimeMultiplayer.leave(getApiClient(), null, mRoomId);
-                getWindow().clearFlags(WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON);
+                Games.RealTimeMultiplayer.leave(mGameHelper.getApiClient(), this, mRoomId);
             } else if (response == GamesActivityResultCodes.RESULT_LEFT_ROOM) {
                 // player wants to leave the room.
-                Games.RealTimeMultiplayer.leave(getApiClient(), null, mRoomId);
-                getWindow().clearFlags(WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON);
+                Games.RealTimeMultiplayer.leave(mGameHelper.getApiClient(), this, mRoomId);
             }
+            //mContext.getWindow().clearFlags(WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON);
+            return true;
+        } else if (request == RC_SELECT_PLAYERS) {
+            if (response != Activity.RESULT_OK) {
+                // user canceled
+                return false;
+            }
+
+            // get the invitee list
+            Bundle extras = data.getExtras();
+            final ArrayList<String> invitees =
+                data.getStringArrayListExtra(Games.EXTRA_PLAYER_IDS);
+
+            // get auto-match criteria
+            Bundle autoMatchCriteria = null;
+            int minAutoMatchPlayers =
+                data.getIntExtra(Multiplayer.EXTRA_MIN_AUTOMATCH_PLAYERS, 0);
+            int maxAutoMatchPlayers =
+                data.getIntExtra(Multiplayer.EXTRA_MAX_AUTOMATCH_PLAYERS, 0);
+
+            if (minAutoMatchPlayers > 0) {
+                autoMatchCriteria =
+                    RoomConfig.createAutoMatchCriteria(
+                            minAutoMatchPlayers, maxAutoMatchPlayers, 0);
+            } else {
+                autoMatchCriteria = null;
+            }
+
+            // create the room and specify a variant if appropriate
+            RoomConfig.Builder roomConfigBuilder = makeBasicRoomConfigBuilder();
+            roomConfigBuilder.addPlayersToInvite(invitees);
+            if (autoMatchCriteria != null) {
+                roomConfigBuilder.setAutoMatchCriteria(autoMatchCriteria);
+            }
+            RoomConfig roomConfig = roomConfigBuilder.build();
+            Games.RealTimeMultiplayer.create(mGameHelper.getApiClient(), roomConfig);
+
+            // prevent screen from sleeping during handshake
+            //getWindow().addFlags(WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON);
         }
-        mGameHelper.onActivityResult(requestCode, resultCode, data);
         return false;
+    }
+
+    @Override
+    public void onRealTimeMessageReceived(RealTimeMessage message) {
+        byte[] data = message.getMessageData();
+        try {
+            String mes = new String(data, "UTF-8");
+            LogD(mes);
+            UserWrapper.onActionResult(mGoogleplay, UserWrapper.ACTION_RET_LOGOUT_SUCCEED, mes);
+        } catch(java.io.UnsupportedEncodingException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void createQuickStartRoom() {
+        PluginWrapper.runOnMainThread(new Runnable() {
+            @Override
+            public void run() {
+                // auto-match criteria to invite one random automatch opponent.  
+                // You can also specify more opponents (up to 3). 
+                Bundle am = RoomConfig.createAutoMatchCriteria(1, 1, 0);
+
+                // build the room config:
+                RoomConfig.Builder roomConfigBuilder = makeBasicRoomConfigBuilder();
+                roomConfigBuilder.setAutoMatchCriteria(am);
+                RoomConfig roomConfig = roomConfigBuilder.build();
+
+                // create room:
+                Games.RealTimeMultiplayer.create(mGameHelper.getApiClient(), roomConfig);
+
+                // prevent screen from sleeping during handshake
+                //mContext.getWindow().addFlags(WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON);
+            }
+        });
+    }
+
+    public RoomConfig.Builder makeBasicRoomConfigBuilder() {
+        return RoomConfig.builder(mGoogleplay)
+            .setMessageReceivedListener(mGoogleplay)
+            .setRoomStatusUpdateListener(mGoogleplay);
+    }
+
+    public void createNormalInviteRoom() {
+        PluginWrapper.runOnMainThread(new Runnable() {
+            @Override
+            public void run() {
+                Intent intent = Games.RealTimeMultiplayer.getSelectOpponentsIntent(mGameHelper.getApiClient(), 1, 1);
+                mContext.startActivityForResult(intent, RC_SELECT_PLAYERS);
+            }
+        });
+    }
+
+    public void leaveRoom() {
+        PluginWrapper.runOnMainThread(new Runnable() {
+            @Override
+            public void run() {
+                Games.RealTimeMultiplayer.leave(mGameHelper.getApiClient(), mGoogleplay, mRoomId);
+            }
+        });
+    }
+
+    public void sendMessage(String message) {
+        try {
+            final byte[] data = message.getBytes("UTF-8");
+            PluginWrapper.runOnMainThread(new Runnable() {
+                @Override
+                public void run() {
+                    Games.RealTimeMultiplayer.sendUnreliableMessageToOthers(mGameHelper.getApiClient(), data, mRoomId);
+                }
+            });
+        } catch(java.io.UnsupportedEncodingException e) {
+            e.printStackTrace();
+        }
     }
 
 }
