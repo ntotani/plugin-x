76b9931f34dac0efbcadfe92c3061a65eabbdf5d
Naoyuki Totani <n_totani@septeni-original.co.jp>
2014-07-31 19:00:55 +0900

    add nend

---
+++ plugins/nend/proj.ios/AdsNend.m
@@ -0,0 +1,118 @@
+/****************************************************************************
+ Copyright (c) 2013 cocos2d-x.org
+ 
+ http://www.cocos2d-x.org
+ 
+ Permission is hereby granted, free of charge, to any person obtaining a copy
+ of this software and associated documentation files (the "Software"), to deal
+ in the Software without restriction, including without limitation the rights
+ to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ copies of the Software, and to permit persons to whom the Software is
+ furnished to do so, subject to the following conditions:
+ 
+ The above copyright notice and this permission notice shall be included in
+ all copies or substantial portions of the Software.
+ 
+ THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ THE SOFTWARE.
+ ****************************************************************************/
+
+#import "AdsNend.h"
+#import "AdsWrapper.h"
+
+#define OUTPUT_LOG(...)     if (self.debug) NSLog(__VA_ARGS__);
+
+@implementation AdsNend
+
+@synthesize debug = __debug;
+
+- (void) dealloc
+{
+    if (self.bannerView != nil) {
+        [self.bannerView setDelegate:nil];
+        [self.bannerView release];
+        self.bannerView = nil;
+    }
+    [super dealloc];
+}
+
+#pragma mark InterfaceAds impl
+
+- (void) configDeveloperInfo: (NSMutableDictionary*) devInfo
+{
+    NSString* apiKey = (NSString*)[devInfo objectForKey:@"ApiKey"];
+    NSString* spotId = (NSString*)[devInfo objectForKey:@"SpotId"];
+    self.bannerView = [[NADView alloc] initWithFrame:CGRectMake(0, 0, 320, 50)];
+    [self.bannerView setIsOutputLog:self.debug];
+    [self.bannerView setNendID:apiKey spotID:spotId];
+    [self.bannerView setDelegate:self];
+    [self.bannerView load];
+}
+
+- (void) showAds: (NSMutableDictionary*) info position:(int) pos
+{
+    [AdsWrapper addAdView:self.bannerView atPos:pos];
+}
+
+- (void) hideAds: (NSMutableDictionary*) info
+{
+    [self.bannerView removeFromSuperview];
+}
+
+- (void) queryPoints
+{
+    OUTPUT_LOG(@"Admob not support query points!");
+}
+
+- (void) spendPoints: (int) points
+{
+    OUTPUT_LOG(@"Admob not support spend points!");
+}
+
+- (void) setDebugMode: (NSNumber*) isDebugMode
+{
+    self.debug = [isDebugMode boolValue];
+    if (self.bannerView) {
+        [self.bannerView setIsOutputLog:self.debug];
+    }
+}
+
+- (NSString*) getSDKVersion
+{
+    return @"2.4.1";
+}
+
+- (NSString*) getPluginVersion
+{
+    return @"0.0.1";
+}
+
+/*
+#pragma mark NADViewDelegate impl
+
+// Since we've received an ad, let's go ahead and set the frame to display it.
+- (void)adViewDidReceiveAd:(GADBannerView *)adView {
+    NSLog(@"Received ad");
+    [AdsWrapper onAdsResult:self withRet:kAdsReceived withMsg:@"Ads request received success!"];
+}
+
+- (void)adView:(GADBannerView *)view didFailToReceiveAdWithError:(GADRequestError *)error {
+    NSLog(@"Failed to receive ad with error: %@", [error localizedFailureReason]);
+    int errorNo = kUnknownError;
+    switch ([error code]) {
+    case kGADErrorNetworkError:
+        errorNo = kNetworkError;
+        break;
+    default:
+        break;
+    }
+    [AdsWrapper onAdsResult:self withRet:errorNo withMsg:[error localizedDescription]];
+    
+}
+*/
+@end
