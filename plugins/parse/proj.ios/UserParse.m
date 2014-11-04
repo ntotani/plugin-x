b365712011201fb45d34c9f87fe622a6ff510720
Naoyuki Totani <nyk.totani@gmail.com>
2014-11-04 23:03:47 +0900

    clean unused method

--- plugins/parse/proj.ios/UserParse.m
+++ plugins/parse/proj.ios/UserParse.m
@@ -105,46 +105,6 @@
     return [PFTwitterUtils twitter].userId;
 }
 
--(PFQuery*)heroineQuery:(NSString*)twID
-{
-    PFQuery *q = [PFQuery queryWithClassName:@"Heroine"];
-    [q whereKey:@"twID" equalTo:@([twID longLongValue])];
-    // TODO enable cache
-    return q;
-}
-
--(NSDictionary*)pfobj2dic:(PFObject*)heroine
-{
-    PFObject *hero = heroine[@"hero"];
-    int turnSec = [self currentTurnMin:heroine] * 60;
-    NSDate *now = [NSDate date];
-    NSDate *dateEnd = [NSDate dateWithTimeInterval:turnSec sinceDate:heroine[@"fsUpdated"]];
-    NSDate *restEnd = [self getRestEnd:heroine[@"twID"] tunrSec:turnSec];
-    NSDate *releaseAt = dateEnd;
-    if ([dateEnd compare:restEnd] == NSOrderedAscending) {
-        releaseAt = restEnd;
-    }
-    BOOL anyFriendShip = [self currentFriendShip:heroine] > 0;
-    BOOL isMyHeroine = anyFriendShip && [hero.objectId isEqualToString:[PFUser currentUser].objectId];
-    int prog = [[self getProgress:[heroine[@"twID"] stringValue]] intValue];
-    return @{
-             @"isMyHeroine":isMyHeroine ? @YES : @NO,
-             @"anyHero":anyFriendShip ? @YES : @NO,
-             @"dateNow":[now compare:dateEnd] == NSOrderedAscending ? @YES : @NO,
-             @"restNow":[now compare:restEnd] == NSOrderedAscending ? @YES : @NO,
-             @"releaseAt":@((int)[releaseAt timeIntervalSince1970]),
-             @"friendShip":@([self currentFriendShip:heroine]),
-             @"okRate":@(100 - 70 * [self currentFriendShip:heroine] / 100),
-             @"broken":prog >= LOVER_PROGRESS && !isMyHeroine ? @YES : @NO,
-             @"turnSec":@(turnSec)};
-}
-
--(NSDate*)getRestEnd:(NSNumber*)twID tunrSec:(int)turnSec
-{
-    int myTouch = [[self getTouch:[twID stringValue]] intValue];
-    return [NSDate dateWithTimeIntervalSince1970:myTouch + turnSec];
-}
-
 - (void)fetchHeroine:(NSString *)ids
 {
     NSMutableArray *numIds = [@[] mutableCopy];
@@ -161,65 +121,6 @@
     [PFCloud callFunctionInBackground:@"heroines" withParameters:@{} block:^(id object, NSError *error) {
         [UserWrapper onActionResult:self withRet:kLogoutSucceed withMsg:object];
     }];
-}
-
-- (NSNumber*)getUserAttr:(NSString*)twID attr:(NSString*)attr
-{
-    if ([PFUser currentUser][attr] && [PFUser currentUser][attr][twID]) {
-        return [PFUser currentUser][attr][twID];
-    }
-    return @0;
-}
-
-- (void)setUserAttr:(NSMutableDictionary*)params attr:(NSString*)attr
-{
-    NSString *twID = params[@"Param1"];
-    int progress = [params[@"Param2"] intValue];
-    NSMutableDictionary *dic = [@{} mutableCopy];
-    if ([PFUser currentUser][attr]) {
-        dic = [[PFUser currentUser][attr] mutableCopy];
-    }
-    if (progress == 0) {
-        [dic removeObjectForKey:twID];
-    } else {
-        dic[twID] = @(progress);
-    }
-    [PFUser currentUser][attr] = dic;
-}
-
-- (NSNumber*)getProgress:(NSString*)twID
-{
-    return [self getUserAttr:twID attr:@"progress"];
-}
-
-- (void)setProgress:(NSMutableDictionary*)params
-{
-    [self setUserAttr:params attr:@"progress"];
-}
-
-- (NSNumber*)getReserve:(NSString*)twID
-{
-    return [self getUserAttr:twID attr:@"reserve"];
-}
-
-- (void)setReserve:(NSMutableDictionary*)params
-{
-    [self setUserAttr:params attr:@"reserve"];
-}
-
-- (NSNumber*)getTouch:(NSString*)twID
-{
-    return [self getUserAttr:twID attr:@"touch"];
-}
-
-- (void)setTouch:(NSMutableDictionary*)params
-{
-    [self setUserAttr:params attr:@"touch"];
-}
-
-- (void)commitUser
-{
-    [[PFUser currentUser] saveInBackground];
 }
 
 - (void)dateHeroine:(NSString*)twID
@@ -249,43 +150,6 @@
 {
     [PFCloud callFunctionInBackground:@"reserve" withParameters:@{@"twID":@([twID intValue])} block:^(id object, NSError *error) {
     }];
-}
-
-- (void)winHeroine:(NSString*)twID
-{
-    [[self heroineQuery:twID] getFirstObjectInBackgroundWithBlock:^(PFObject *object, NSError *error) {
-        if (!object) {
-            object = [PFObject objectWithClassName:@"Heroine"];
-            object.ACL = [PFACL ACL];
-            [object.ACL setPublicReadAccess:YES];
-            [object.ACL setPublicWriteAccess:YES];
-            object[@"twID"] = @([twID longLongValue]);
-            object[@"turnMin"] = @(TURNMIN_DEFAULT);
-        }
-        object[@"friendShip"] = @100;
-        object[@"fsUpdated"] = [NSDate date];
-        object[@"hero"] = [PFUser currentUser];
-        [self setProgress:[@{@"Param1":twID, @"Param2":@(LOVER_PROGRESS)} mutableCopy]];
-        NSMutableDictionary *reset = [@{@"Param1":twID, @"Param2":@0} mutableCopy];
-        [self setReserve:reset];
-        [self setTouch:reset];
-        [PFObject saveAllInBackground:@[object, [PFUser currentUser]]];
-    }];
-}
-
--(int)currentTurnMin:(PFObject*)heroine
-{
-    int pastSec = [[NSDate date] timeIntervalSinceDate:heroine[@"tmUpdated"]];
-    int turnMin = [heroine[@"turnMin"] intValue] + TURNMIN_DEFAULT * pastSec / 259200;
-    return MIN(turnMin, TURNMIN_DEFAULT);
-}
-
-- (int)currentFriendShip:(PFObject*)heroine
-{
-    int pastMin = [[NSDate date] timeIntervalSinceDate:heroine[@"fsUpdated"]] / 60;
-    int pastTurn = pastMin / [self currentTurnMin:heroine];
-    int friendShip = [heroine[@"friendShip"] intValue] - pastTurn * 4;
-    return MAX(friendShip, 0);
 }
 
 - (void)touchHeroine:(NSString*)twID
